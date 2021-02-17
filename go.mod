ab3a1b9f1f159095cf32b0a98ff1e078bbb1392b
Jacob Gadikian <jacobgadikian@gmail.com>
2021-02-17 19:24:15 +0700

    make it a real chain, please

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/fivesixnine/pc-demo
 go 1.15
 
 require (
+	github.com/CosmWasm/wasmd v0.14.1-0.20210113091717-f9015cba4793
 	github.com/cosmos/cosmos-sdk v0.41.0
 	github.com/gogo/protobuf v1.3.3
+	github.com/golang/protobuf v1.4.3
 	github.com/gorilla/mux v1.8.0
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
 	github.com/spf13/cast v1.3.1
--- x/pcdemo/client/rest/rest.go
+++ x/pcdemo/client/rest/rest.go
@@ -14,12 +14,22 @@ const (
 // RegisterRoutes registers pcdemo-related REST handlers to a router
 func RegisterRoutes(clientCtx client.Context, r *mux.Router) {
 	// this line is used by starport scaffolding # 2
+	registerQueryRoutes(clientCtx, r)
+	registerTxHandlers(clientCtx, r)
+
 }
 
 func registerQueryRoutes(clientCtx client.Context, r *mux.Router) {
 	// this line is used by starport scaffolding # 3
+	r.HandleFunc("/pcdemo/posts/{id}", getPostHandler(clientCtx)).Methods("GET")
+	r.HandleFunc("/pcdemo/posts", listPostHandler(clientCtx)).Methods("GET")
+
 }
 
 func registerTxHandlers(clientCtx client.Context, r *mux.Router) {
 	// this line is used by starport scaffolding # 4
+	r.HandleFunc("/pcdemo/posts", createPostHandler(clientCtx)).Methods("POST")
+	r.HandleFunc("/pcdemo/posts/{id}", updatePostHandler(clientCtx)).Methods("POST")
+	r.HandleFunc("/pcdemo/posts/{id}", deletePostHandler(clientCtx)).Methods("POST")
+
 }
